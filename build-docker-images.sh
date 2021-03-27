3e40a9ae14a3ec9a520feadcddfd856a806c2165
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2021-03-28 01:53:43 +0900

    add build-docker-image

---
+++ build-docker-images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] ; then
+  echo "Error! No tag is specified!"
+  echo "Usage: $0 tag"
+  exit 1
+fi
+
+tag=$1
+
+# build image
+docker build -t docker-fix:${tag} .
+
+# make archive
+docker save -o docker-fix.${tag}.tar docker-fix:${tag}
+pigz docker-fix.${tag}.tar
+
